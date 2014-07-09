9b22c2ab8d8f1c42b7b756b844ffb1761d6b75b8
catearcher <indieboy86@gmail.com>
2014-07-09 14:24:59 +0200

    tidy up

--- theme-now/magic.js
+++ theme-now/magic.js
@@ -8,19 +8,28 @@
   createPostHtml = function(post) {
     return WallStream.tmpl($("#post-template").html(), post);
   },
+  preparePost = function(post) {
+    if (post.comment.length > 80) {
+      post.comment = post.comment.substr(0, 77) + "…";
+    }
+
+    return post;
+  },
+  insertPost = function(post) {
+    $(".wall").prepend(createPostHtml(post));
+  },
+  pruneOldPosts = function() {
+    $(".post").slice(20).remove();
+  },
   stream = new WallStreamCore({
     accessToken: "5f864451221b0e8d2ff61b3179ac1a3b5d4ac9e3",
     fields: ["id", "type", "external_name", "comment", "post_image"],
     onPost: function(post) {
       console.log(post);
 
-      if (post.comment.length > 80) {
-        post.comment = post.comment.substr(0, 77) + "…";
-      }
-
-      $(".wall")
-      .prepend(createPostHtml(post))
-      .find(".post").slice(20).remove();
+      post = preparePost(post);
+      insertPost(post);
+      pruneOldPosts();
     }
   });
 }());
