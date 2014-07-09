14dc7343173613e104cff8d47d7a75b96ad45a23
catearcher <indieboy86@gmail.com>
2014-07-09 14:20:16 +0200

    only load needed fields

--- theme-now/magic.js
+++ theme-now/magic.js
@@ -10,6 +10,7 @@
   },
   stream = new WallStreamCore({
     accessToken: "5f864451221b0e8d2ff61b3179ac1a3b5d4ac9e3",
+    fields: ["id", "type", "external_name", "comment", "post_image"],
     onPost: function(post) {
       console.log(post);
 
